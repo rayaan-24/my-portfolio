43c9ca27f2969e698b2cf6879b05fd6d82401caf
rayaan-24 <mohammedrayaann@gmail.com>
2025-12-27 01:28:03 +0530

    changed the text in about

--- src/components/About/Github.js
+++ src/components/About/Github.js
@@ -12,7 +12,7 @@ function Github() {
       }}
     >
       <h1 className="project-heading pb-4" style={{ paddingBottom: "20px" }}>
-        Days I <strong className="purple">Code</strong>
+        Days I <strong className="purple">Push Code in Github</strong>
       </h1>
       <GitHubCalendar
         username="rayaan-24"
